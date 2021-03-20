2c781167a4328a48b0b5041915a9b7ce57b81617
DAGNOGO Jean-François <jdqgnogo11@gmail.com>
2021-03-20 10:21:56 +0200

    init package and dependency

--- app/src/main/java/com/jdagnogo/lydiausers/ui/MainActivity.kt
+++ app/src/main/java/com/jdagnogo/lydiausers/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jdagnogo.lydiausers
+package com.jdagnogo.lydiausers.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdagnogo.lydiausers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
